ac67d78e749dd03065851d95e8ee37b0758875c9
JStone1 <62293789+JStone1@users.noreply.github.com>
2022-05-22 22:19:47 +0100

    Final commit?

--- scripts.js
+++ scripts.js
@@ -1,36 +1,3 @@
-function showModel1() {
-  document.getElementById("model").src =
-    "https://sketchfab.com/models/60e8c68180094d21a83f2340aa8d6013/embed";
-  document
-    .getElementsByClassName("thumbnail")[1]
-    .classList.add("thumbnail-disabled");
-  document
-    .getElementsByClassName("thumbnail")[0]
-    .classList.remove("thumbnail-disabled");
-  document
-    .getElementsByClassName("model-arrow")[0]
-    .classList.add("model-arrow-disabled");
-  document
-    .getElementsByClassName("model-arrow")[1]
-    .classList.remove("model-arrow-disabled");
-}
-function showModel2() {
-  document.getElementById("model").src =
-    "https://sketchfab.com/models/bf69eb00eaba4df3851b510f941a3c7a/embed";
-  document.getElementById("model").title = "Military telescope";
-  document
-    .getElementsByClassName("thumbnail")[0]
-    .classList.add("thumbnail-disabled");
-  document
-    .getElementsByClassName("thumbnail")[1]
-    .classList.remove("thumbnail-disabled");
-  document
-    .getElementsByClassName("model-arrow")[1]
-    .classList.add("model-arrow-disabled");
-  document
-    .getElementsByClassName("model-arrow")[0]
-    .classList.remove("model-arrow-disabled");
-}
 function openNav() {
   document
     .getElementsByClassName("nav-container")[0]
@@ -56,43 +23,6 @@ function createStars() {
       section.appendChild(star);
     }
   }
-}
-
-function changeModel() {
-  let counter = 0;
-  document
-    .getElementById("carousel-controls")
-    .addEventListener("click", function (e) {
-      let children = document.getElementById("carousel").children;
-      let element = e.target;
-      let action = element.dataset.action;
-
-      if (action) {
-        if (action == "prev") {
-          if (counter != 0) {
-            counter--;
-          }
-        } else {
-          if (counter < 1) {
-            counter++;
-          }
-        }
-      }
-
-      document
-        .getElementById("carousel")
-        .children[counter].scrollIntoView({ behavior: "smooth" });
-      console.log(counter);
-
-      let video = document.getElementById("video");
-      video.pause();
-    });
-  document
-    .getElementsByClassName("thumbnail")[1]
-    .classList.add("thumbnail-disabled");
-  document
-    .getElementsByClassName("thumbnail")[0]
-    .classList.remove("thumbnail-disabled");
 }
 
 function showVideo() {
